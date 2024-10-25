eb57af9bcd8638b405322549f81dc354c72fca42
Jehad Atieh <jehadatieh20@gmail.com>
2024-10-26 02:24:02 +0300

    localizations home layout Screen

--- lib/features/layout/layout.dart
+++ lib/features/layout/layout.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import '../../core/utils/constants/colors.dart';
-import '../../core/utils/constants/text_strings.dart';
 import '../chatbot/presentation/views/chatbot_view.dart';
 import '../home/presentation/views/home_view.dart';
 import '../notification/presentation/views/notification_view.dart';
 import '../profile/presentation/views/profile_view.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 
 class LayoutView extends StatefulWidget {
   const LayoutView({super.key});
@@ -16,6 +17,7 @@ class LayoutView extends StatefulWidget {
 }
 
 class _LayoutViewState extends State<LayoutView> {
+
   var _currentIndex = 0;
   List screens = const [
     HomeView(),
@@ -25,6 +27,7 @@ class _LayoutViewState extends State<LayoutView> {
   ];
   @override
   Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context)!;
     return Scaffold(
       body: screens[_currentIndex],
       bottomNavigationBar: SalomonBottomBar(
@@ -37,22 +40,22 @@ class _LayoutViewState extends State<LayoutView> {
         items: [
           SalomonBottomBarItem(
             icon: const Icon(Iconsax.home_24),
-            title: const Text(QTexts.homeAppbarTitle),
+            title:  Text(localizations.homeScreen),
             selectedColor: QColors.secondary,
           ),
           SalomonBottomBarItem(
             icon: const Icon(Iconsax.message_text_15),
-            title: const Text(QTexts.chatbotAppbarTitle),
+            title:  Text(localizations.chatbotAppbarTitle),
             selectedColor: QColors.secondary,
           ),
           SalomonBottomBarItem(
             icon: const Icon(Iconsax.notification_bing5),
-            title: const Text(QTexts.notificationAppbarTitle),
+            title:  Text(localizations.notificationScreen),
             selectedColor: QColors.secondary,
           ),
           SalomonBottomBarItem(
             icon: const Icon(Icons.person),
-            title: const Text(QTexts.profileAppbarTitle),
+            title:  Text(localizations.profileScreen),
             selectedColor: QColors.secondary,
           ),
         ],
