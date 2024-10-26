2279c2a1f5badaaa43aa63a283ce37e3a1dc159f
Jehad Atieh <jehadatieh20@gmail.com>
2024-10-26 03:31:31 +0300

    localizations Privacy Policy Settings Screen

--- lib/features/profile/presentation/views/widgets/privacy_policy_widgets.dart/cutsom_privacy_policy_app_bar.dart
+++ lib/features/profile/presentation/views/widgets/privacy_policy_widgets.dart/cutsom_privacy_policy_app_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../../../../../core/utils/constants/text_strings.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../../../../../core/utils/styles.dart';
 
 class CustomPrivacyPolicyAppBar extends StatelessWidget {
@@ -8,6 +8,7 @@ class CustomPrivacyPolicyAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context)!;
     return Padding(
       padding: const EdgeInsets.only(right: 8, left: 8, top: 40, bottom: 10),
       child: Row(
@@ -19,7 +20,7 @@ class CustomPrivacyPolicyAppBar extends StatelessWidget {
             icon: const Icon(Icons.arrow_back_ios),
           ),
           Text(
-            QTexts.privacyPolicyAppBarTitle,
+            localizations.loginPrivacyPolicy,
             style: Styles.textStyle26.copyWith(
               fontWeight: FontWeight.bold,
             ),
--- lib/features/profile/presentation/views/widgets/privacy_policy_widgets.dart/privacy_policy_details_body.dart
+++ lib/features/profile/presentation/views/widgets/privacy_policy_widgets.dart/privacy_policy_details_body.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
-import '../../../../../../core/utils/constants/text_strings.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class PrivacyPolicyDetailsBody extends StatelessWidget {
   const PrivacyPolicyDetailsBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    final localizations = AppLocalizations.of(context)!;
+    return  Padding(
       padding: EdgeInsets.symmetric(horizontal: 14, vertical: 24),
       child: Column(
         children: [
           Text(
             textAlign: TextAlign.center,
-            QTexts.aboutUsBodyText,
+            localizations.aboutUsBodyText,
           ),
           Text(
             textAlign: TextAlign.center,
-            QTexts.aboutUsBodyText,
+            localizations.aboutUsBodyText,
           ),
         ],
       ),
