47cf0a0746dd633bcec5dd74dabaa594b0bc2813
Jehad Atieh <jehadatieh20@gmail.com>
2024-10-26 03:14:14 +0300

    localizations History Screen

--- lib/features/profile/presentation/views/widgets/history_widgets/custom_history_app_bar.dart
+++ lib/features/profile/presentation/views/widgets/history_widgets/custom_history_app_bar.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../../../../../core/utils/constants/text_strings.dart';
 import '../../../../../../core/utils/styles.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 
 class CustomHistoryAppBar extends StatelessWidget {
   const CustomHistoryAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context)!;
     return Padding(
       padding: const EdgeInsets.only(right: 8, left: 8, top: 40, bottom: 10),
       child: Row(
@@ -19,7 +21,7 @@ class CustomHistoryAppBar extends StatelessWidget {
             icon: const Icon(Icons.arrow_back_ios),
           ),
           Text(
-            QTexts.historyAppBarTitle,
+            localizations.historyAppBarTitle,
             style: Styles.textStyle26.copyWith(
               fontWeight: FontWeight.bold,
             ),
