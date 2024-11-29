1e611f4549c3b886596c1eb4ee046cf758f7c61e
Mohammed Jaber <108561613+mjaber5@users.noreply.github.com>
2024-11-29 23:11:12 +0300

    fix notification content

--- lib/features/notification/presentation/views/notification_view.dart
+++ lib/features/notification/presentation/views/notification_view.dart
@@ -1,36 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:qanoni/features/notification/presentation/views/widgets/widget/screnhome.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-import '../../../../core/utils/styles.dart';
-
-
+import 'package:qanoni/features/notification/presentation/views/widgets/notification_view_body.dart';
 
 class NotificationView extends StatelessWidget {
   const NotificationView({super.key});
 
   @override
   Widget build(BuildContext context) {
-        final localizations = AppLocalizations.of(context)!;
-
-    return  Scaffold(
-      appBar: AppBar(title: 
-      Padding(
-      padding: const EdgeInsets.only(right: 24, left: 14, top: 15, bottom: 10),
-      child: Row(
-        children: [
-          Text(
-            localizations.notificationScreen,
-            style: Styles.textStyle30,
-            textAlign: TextAlign.center,
-          ),
-        ],
-      ),
-    ),),
-       body: const Padding(
-         padding: EdgeInsets.only(top: 20),
-         child: Screnhome(),
-       ),
+    return const Scaffold(
+      body: NotificationViewBody(),
     );
   }
 }
--- lib/features/notification/presentation/views/widgets/notification_view_body.dart
+++ lib/features/notification/presentation/views/widgets/notification_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:qanoni/features/notification/presentation/views/widgets/custom_notification_app_bar.dart';
 
-import 'custom_notification_app_bar.dart';
-import 'widget/screnhome.dart';
+import 'notification_content.dart';
 
 class NotificationViewBody extends StatelessWidget {
   const NotificationViewBody({
@@ -13,8 +13,13 @@ class NotificationViewBody extends StatelessWidget {
     return const Column(
       children: [
         AppbarNotification(),
-         Screnhome()     
-         ],
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 4.0),
+            child: NotificationContent(),
+          ),
+        ),
+      ],
     );
   }
 }
