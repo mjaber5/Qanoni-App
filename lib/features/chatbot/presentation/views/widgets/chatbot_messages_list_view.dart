832d0c9fd323cdd658198c8838ecba515010d5c2
mo7md27 <mohammadabushera77@gmail.com>
2024-12-25 20:38:25 +0300

    edit ui chatbot

--- lib/features/chatbot/presentation/views/widgets/chatbot_messages_list_view.dart
+++ lib/features/chatbot/presentation/views/widgets/chatbot_messages_list_view.dart
@@ -15,12 +15,12 @@ class ChatbotMessagesListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: DashChat(
-        currentUser: ChatUser(id: '1', firstName: 'Lawyer AI'),
+        currentUser: ChatUser(id: '1', firstName: 'Useredit'),
         typingUsers: typingUsers, // هنا تم تمرير قائمة الـ typingUsers
-        // messageOptions: MessageOptions(
-        //   currentUserContainerColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.6),
-        //   currentUserTextColor: Colors.white,
-        // ),
+        messageOptions: MessageOptions(
+          currentUserContainerColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.6),
+          currentUserTextColor: Colors.white,
+        ),
         messages: _messages.map((message) {
           return ChatMessage(
             text: message['text'],
