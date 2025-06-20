f5064640cd6fba6e7596d22a4a19b1ee4969de26
Mohammed Jaber <108561613+mjaber5@users.noreply.github.com>
2025-06-20 17:16:03 +0300

    update

--- lib/core/services/base.dart
+++ lib/core/services/base.dart
@@ -1,4 +1,4 @@
 class ConfigApi {
   static const baseUri =
-      'https://53dc-2a01-9700-44c5-fa00-6ceb-6040-3da7-70ae.ngrok-free.app';
+      'https://790f-2a01-9700-4404-a600-122-788e-afac-c984.ngrok-free.app';
 }
